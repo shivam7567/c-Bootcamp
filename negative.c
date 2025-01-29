03bdbcc01d1237b01403ac829084d13a5ae031c2
shivam7567 <shivamsinghtomar756@gmail.com>
2025-01-30 02:59:44 +0530

    Add files via upload

--- negative.c
+++ negative.c
@@ -2,12 +2,8 @@
 
 int main() {
     int num;
-
-    // Taking user input
     printf("Enter a number: ");
     scanf("%d", &num);
-
-    // Checking the number
     if (num > 0) {
         printf("The number is positive.\n");
     } else if (num < 0) {
