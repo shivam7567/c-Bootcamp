954857874b06d84ba791692d0237dc88f84d3937
shivam7567 <shivamsinghtomar756@gmail.com>
2025-01-30 02:58:51 +0530

    Add files via upload

--- vowel.c
+++ vowel.c
@@ -2,12 +2,8 @@
 
 int main() {
     char ch;
-
-    // Taking user input
     printf("Enter a character: ");
     scanf("%c", &ch);
-
-    // Checking if the character is a vowel or consonant
     if ((ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') || 
         (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')) {
         printf("%c is a vowel.\n", ch);
