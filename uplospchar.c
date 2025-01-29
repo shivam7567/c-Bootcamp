cfe00eebf0c86d2b1cde951e24a55c4a5f67e291
shivam7567 <shivamsinghtomar756@gmail.com>
2025-01-30 03:05:32 +0530

    Add files via upload

---
+++ uplospchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+    if (ch >= 'A' && ch <= 'Z') {
+        printf("%c is an uppercase letter.\n", ch);
+    } 
+    else if (ch >= 'a' && ch <= 'z') {
+        printf("%c is a lowercase letter.\n", ch);
+    } 
+    else {
+        printf("%c is a special character.\n", ch);
+    }
+    return 0;
+}
